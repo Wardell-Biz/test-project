09ad7fd185406b8e5acbbc30d74aaae745baf168
Kawsar Ahmed <prokawsar@gmail.com>
2023-03-24 10:31:20 +0600

    Table component

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,26 @@
 <script>
 	// import Component1 from './Component1.svelte';
 	// import Component2 from './Component2.svelte';
-
+	import Table from "./Table.svelte";
 	export let name;
+
+	const data = [
+    { id: 1, name: 'Load balancer 1', protocol: 'HTTP', port: 300, rule: 'Round Robin', attached_group: 'VM Groups', status: 'Starting' },
+    { id: 2, name: 'Load balancer 2', protocol: 'HTTPS', port: 443, rule: 'Least Connections', attached_group: 'Container Groups', status: 'Running' },
+    { id: 3, name: 'Load balancer 3', protocol: 'TCP', port: 8080, rule: 'Source IP Hash', attached_group: 'VM Groups', status: 'Stopping' },
+    { id: 4, name: 'Load balancer 4', protocol: 'HTTP', port: 80, rule: 'Least Connections', attached_group: 'Container Groups', status: 'Stopped' },
+    { id: 5, name: 'Load balancer 5', protocol: 'TCP', port: 3306, rule: 'Round Robin', attached_group: 'Database Servers', status: 'Error' },
+  ];
+
+  const header = ['ID', 'Name', 'Protocol', 'Port', 'Rule', 'Attached Group', 'Status'];
+
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-
-	<!-- <Component1 /> -->
-	<!-- <Component2 /> -->
+	<div class="table-container px-10">
+		<Table rows={data} header={header} />
+	</div>
 </main>
 
 <style>
@@ -31,5 +42,15 @@
 		main {
 			max-width: none;
 		}
+	}
+
+	.table-container {
+    display: flex;
+    justify-content: center;
+  }
+
+	.px-10{
+		padding-left: 5rem;
+		padding-right: 5rem;
 	}
 </style>
---
+++ src/Table.svelte
@@ -0,0 +1,49 @@
+<script>
+  export let rows = [];
+  export let header = [];
+</script>
+
+<table>
+  <thead>
+    <tr>
+      {#each header as column}
+        <th>{column}</th>
+      {/each}
+    </tr>
+  </thead>
+  <tbody>
+    {#each rows as row}
+      <tr>
+        {#each Object.values(row) as value}
+          <td>{value}</td>
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
+
+<style>
+  table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+  
+  th {
+    background-color: #cfcccc;
+    font-weight: bold;
+    padding: 8px;
+    text-align: left;
+  }
+  
+  td {
+    border-bottom: 1px solid #d6d2d2;
+    padding: 8px;
+    text-align: left;
+		background-color: #f1eded;
+  }
+  
+  /* tr:nth-child(even) {
+    background-color: #f9f9f9;
+  } */
+</style>
